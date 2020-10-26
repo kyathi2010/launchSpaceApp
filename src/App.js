e5b03523824e54e420713190f32667693a8308b7
kyathi <kkanumuri@deloitte.com>
2020-10-26 14:44:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { createStore,applyMiddleware,compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+import { fetchAllData } from './actions/launchAction'
+import AppRoute from "./AppRoute";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+store.dispatch(fetchAllData())
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+       <AppRoute />
+    </Provider>
   );
 }
 
---
+++ src/components/home/home.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Filters from '../filters';
+import Launches from '../launches';
+import  './home.css';
+class Home extends Component {
+  componentDidMount(){
+    document.body.style.backgroundColor = "#80808040";
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <b>SpaceX Launch Programs</b>
+        </header>
+        <Filters />
+        <Launches />
+        <footer>
+          <b>Developed by:</b>
+          <p className="developerName">Kyathi Kanumuri</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Home;
